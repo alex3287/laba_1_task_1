20e39758cc30ad39d73d90eb4fefe79cab90ff69
Alexander Mangazeev <alex3287@bk.ru>
2020-11-03 00:50:07 +0700

    change main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include "classb.h"
+#include "classa.h"
+
 using namespace std;
 
 int main()
 {
-
+    ClassB b;
+    cout<<b.getX()<<endl;
     cout<<"hello world"<<endl;
     return 0;
 }
